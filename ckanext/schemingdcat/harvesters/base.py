9074fed29aa1bfacbb57404460cc0a840947c081
mjanez <96422458+mjanez@users.noreply.github.com>
2024-04-11 08:35:14 +0200

    Fix accented character handling in SchemingDCATHarvester

--- ckanext/schemingdcat/harvesters/base.py
+++ ckanext/schemingdcat/harvesters/base.py
@@ -1162,17 +1162,36 @@ class SchemingDCATHarvester(HarvesterBase):
         # Define a dictionary to map accented characters to their unaccented equivalents except ñ
         accent_map = {
             "á": "a",
+            "à": "a",
+            "ä": "a",
+            "â": "a",
+            "ã": "a",
             "é": "e",
+            "è": "e",
+            "ë": "e",
+            "ê": "e",
             "í": "i",
+            "ì": "i",
+            "ï": "i",
+            "î": "i",
             "ó": "o",
+            "ò": "o",
+            "ö": "o",
+            "ô": "o",
+            "õ": "o",
             "ú": "u",
+            "ù": "u",
             "ü": "u",
+            "û": "u",
             "ñ": "ñ",
         }
 
+        # Convert the name to lowercase
+        name = name.lower()
+
         # Replace accented and special characters with their unaccented equivalents or -
         name = "".join(accent_map.get(c, c) for c in name)
-        name = re.sub(r"[^a-zñ0-9_.-]", "-", name.lower().strip())
+        name = re.sub(r"[^a-zñ0-9_.-]", "-", name.strip())
 
         # Truncate the name to 40 characters
         name = name[:40]
