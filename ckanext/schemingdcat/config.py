fcd7bbe09b2eacf7bee4dbec922a2793cc7c5d4c
mjanez <96422458+mjanez@users.noreply.github.com>
2024-05-28 12:46:20 +0200

    Add icons to package items to render theme/spatial_uri

--- ckanext/schemingdcat/config.py
+++ ckanext/schemingdcat/config.py
@@ -11,6 +11,8 @@ linkeddata_links = None
 geometadata_links = None
 endpoints = None
 facet_list_limit= 6
+default_package_item_icon = 'theme'
+default_package_item_show_spatial = True
 
 # Default DCAT metadata configuration
 OGC2CKAN_HARVESTER_MD_CONFIG = {
